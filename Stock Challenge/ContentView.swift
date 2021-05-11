b6d2515f4a8c994fd1e845cda17ca105ce267cc2
Tomasz Kubicki <tjpk@poczta.onet.pl>
2021-05-11 10:32:28 +0200

    Initial Commit

---
+++ Stock Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock Challenge
+//
+//  Created by MacBook Pro (Tomasz) on 11/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stock Challenge/Stock_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_ChallengeApp.swift
+//  Stock Challenge
+//
+//  Created by MacBook Pro (Tomasz) on 11/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Stock_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
